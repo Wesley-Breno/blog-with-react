bdc96a375503025e5e903cdbfc3c7e71e1509ac5
wesley <wesleybreno074@gmail.com>
2026-03-22 20:39:03 -0300

    class 105

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,17 @@
-export default function Home() {
-  return <h1>Hello, World!</h1>
+import clsx from "clsx";
+
+export default function HomePage() {
+  return (
+    <>
+      <h1
+        className={clsx(
+          "text-6xl font-bold text-blue-500",
+          "hover:text-blue-50 hover:bg-blue-500",
+          "transition duration-1000"
+        )}
+      >
+        Texto no meu H1
+      </h1>
+    </>
+  );
 }
