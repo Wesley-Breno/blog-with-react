5573e5afcf7b80282fb52d09dd9195b9045cc902
wesley <wesleybreno074@gmail.com>
2026-03-22 20:50:05 -0300

    class 106

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
-import clsx from "clsx";
+import { Header } from "./components/Header";
 
-export default function HomePage() {
+export default async function HomePage() {
   return (
     <>
-      <h1
-        className={clsx(
-          "text-6xl font-bold text-blue-500",
-          "hover:text-blue-50 hover:bg-blue-500",
-          "transition duration-1000"
-        )}
-      >
-        Texto no meu H1
-      </h1>
+      <Header />
     </>
   );
 }
