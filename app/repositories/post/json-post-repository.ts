53379e988d9c380cd2895ee70387adbaea2cf2a8
wesley <wesleybreno074@gmail.com>
2026-03-22 22:27:16 -0300

    class 110

--- app/repositories/post/json-post-repository.ts
+++ app/repositories/post/json-post-repository.ts
@@ -1,14 +1,32 @@
 import { PostModel } from "@/app/models/post/post-model";
 import { PostRepository } from "@/app/repositories/post/post-repository";
+import { resolve } from "path";
+import { readFile } from "fs/promises";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(ROOT_DIR, "app", "db", "seed", "posts.json");
 
 export class JsonPostRepository implements PostRepository {
-    private async readFromDisk() {}
+    private async readFromDisk() : Promise<PostModel[]> {
+        const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+        const parsedJson = JSON.parse(jsonContent);
+        const { posts } = parsedJson;
+        return posts;
+    }
 
-    async findAll(): Promise<PostModel[]> {}
+    async findAll(): Promise<PostModel[]> {
+        const posts = await this.readFromDisk();
+        return posts;
+    }
+
+    async findById(id: string): Promise<PostModel> {
+        const posts = await this.readFromDisk();
+        const post = posts.find((post) => post.id === id);
+
+        if (!post) throw new Error(`Post with id ${id} not found`);
+
+        return post;
+    }
 }
 
-export const postRepository = new JsonPostRepository();
-
-console.log("ROOT_DIR", ROOT_DIR);
+export const postRepository: PostRepository = new JsonPostRepository();
