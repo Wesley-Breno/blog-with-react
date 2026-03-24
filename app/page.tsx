295df3a2b9f16ac3e10a4378936602d6711adfc7
wesley <wesleybreno074@gmail.com>
2026-03-24 03:02:06 -0300

    class 116

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,13 @@ import { Suspense } from "react";
 import { PostsList } from "./components/PostsList";
 import { SpinLoader } from "./components/SpinLoader";
 import { Container } from "./components/Container";
+import { Header } from "./components/Header";
 
 export default async function HomePage() {
   return (
     <>
       <Container>
-        <header>
-          <h1 className="text-6xl font-bold text-center py-8">
-            Aqui é a HEADER
-          </h1>
-
-          <p className="text-justify">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Culpa
-            sapiente corrupti esse iure! Quos, atque. Vero veritatis iure
-            incidunt totam provident laudantium quo quidem iusto dolorem!
-            Dolorum officiis molestiae rem?
-          </p>
-        </header>
+        <Header />
 
         <Suspense fallback={<SpinLoader />}>
           <PostsList />
