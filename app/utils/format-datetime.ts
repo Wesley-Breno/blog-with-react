3e83473048e7565d05481e7095db5f94435269c5
wesley <wesleybreno074@gmail.com>
2026-03-24 05:13:31 -0300

    class 123

---
+++ app/utils/format-datetime.ts
@@ -0,0 +1,19 @@
+import { format, formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale/pt-BR";
+
+export function formateDatetime (rawDate: string): string {
+    const date = new Date(rawDate);
+    
+    return format(date, "dd/MM/yyyy 'às' HH'h'mm", {
+        locale: ptBR,
+    });
+}
+
+export function formateRelativeDate (rawDate: string): string {
+    const date = new Date(rawDate);
+    
+    return formatDistanceToNow(date, {
+        locale: ptBR,
+        addSuffix: true,
+    });
+}
