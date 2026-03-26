9a4f9abb99dbcdeb04054754fdc289c693fbb2d0
wesley <wesleybreno074@gmail.com>
2026-03-26 17:51:53 -0300

    class 138

--- app/components/SafeMarkdown/index.tsx
+++ app/components/SafeMarkdown/index.tsx
@@ -20,12 +20,23 @@ export function SafeMarkdown({ markdown }: SafeMarkdownProps) {
         "prose-a:hover:text-blue-700",
         "prose-a:hover:underline",
         "prose-img:mx-auto",
-        "lg:prose-lg"
+        "lg:prose-lg",
       )}
     >
       <ReactMarkdown
         rehypePlugins={[rehypeSanitize]}
         remarkPlugins={[remarkGfm]}
+        components={{
+          table: ({ node, ...props }) => {
+            if (!node?.children) return "";
+
+            return (
+              <div className="overflow-x-auto">
+                <table className="w-full min-w-[600px]" {...props} />
+              </div>
+            );
+          },
+        }}
       >
         {markdown}
       </ReactMarkdown>
