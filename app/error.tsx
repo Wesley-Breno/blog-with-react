d17afde99f26e0cf55235759822f29bd62fb6fa6
wesley <wesleybreno074@gmail.com>
2026-03-26 21:28:57 -0300

    class 141

--- app/error.tsx
+++ app/error.tsx
@@ -10,7 +10,7 @@ type RootErrorPageProps = {
 
 export default function RootErrorPage({error}: RootErrorPageProps) {
   useEffect(() => {
-    // console.error(error);
+    console.error(error);
   }, [error]);
 
   return (
