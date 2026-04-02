6bb552df4337ef2f056f04c7704b0dd688601100
wesley <wesleybreno074@gmail.com>
2026-04-02 05:32:13 -0300

    class 164

--- app/lib/post/queries/public.ts
+++ app/lib/post/queries/public.ts
@@ -3,30 +3,32 @@ import { unstable_cache } from 'next/cache';
 import { notFound } from 'next/navigation';
 import { cache } from 'react';
 
-export const findAllPublicPostsCached = unstable_cache(
-  cache(async () => {
+export const findAllPublicPostsCached = cache(unstable_cache(
+  async () => {
     return await postRepository.findAllPublic();
-  }),
+  },
   ['posts'],
   {
     tags: ['posts'],
   },
-);
+));
 
-export const findPublicPostBySlugCached = (slug: string) =>
-  unstable_cache(
-    cache(async (slug: string) => {
+export const findPublicPostBySlugCached = cache((slug: string) => {
+  return unstable_cache(
+    async (slug: string) => {
       const post = await postRepository
-        .findBySlugPublic(slug)
-        .catch(() => undefined);
+      .findBySlugPublic(slug)
+      .catch(() => undefined);
 
-      if (!post) notFound();
-
+      if (!post) {
+        notFound();
+      }
+      
       return post;
-    }),
-    ['posts'],
+    },
+    [`post-${slug}`],
     {
       tags: [`post-${slug}`],
-    },
+    }
   )(slug);
-
+})
