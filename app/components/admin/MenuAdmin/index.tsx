00537967ef7c002689668cb85ac2d0d3701728db
wesley <wesleybreno074@gmail.com>
2026-04-04 16:50:58 -0300

    class 175

--- app/components/admin/MenuAdmin/index.tsx
+++ app/components/admin/MenuAdmin/index.tsx
@@ -1,25 +1,61 @@
+"use client";
+
 import clsx from "clsx";
-import { FileTextIcon, HouseIcon } from "lucide-react";
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function MenuAdmin() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   const navClasses = clsx(
     "bg-slate-900 text-slate-100 rounded-lg",
     "flex flex-col mb-8",
-    'sm:flex-row sm:flex-wrap',
-    // 'h-10'
-    // 'overflow-hidden'
+    "sm:flex-row sm:flex-wrap",
+    !isOpen && "h-10",
+    !isOpen && "overflow-hidden",
+    "sm:overflow-visible sm:h-auto",
   );
   const linkClasses = clsx(
     "[&>svg]:w-[16px] [&>svg]:h-[16px] px-4",
-    "flex items-center justify-start gap-2",
+    "flex items-center justify-start gap-2 cursor-pointer",
     "transition hover:bg-slate-800 rounded-lg",
-    'h-10',
-    'shrink-0'
+    "h-10",
+    "shrink-0",
+  );
+  const openCloseBtnClasses = clsx(
+    linkClasses,
+    "text-blue-200 italic",
+    "sm:hidden",
   );
 
   return (
     <nav className={navClasses}>
+      <button
+        onClick={() => setIsOpen((s) => !s)}
+        className={openCloseBtnClasses}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Menu
+          </>
+        )}
+
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Fechar
+          </>
+        )}
+      </button>
+
       <a href="/" target="_blank" className={linkClasses}>
         <HouseIcon />
         Home
