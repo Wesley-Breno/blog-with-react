5e1347b3d5a47882318b8c07a40b15d2eea18db9
wesley <wesleybreno074@gmail.com>
2026-04-09 20:56:57 -0300

    class 178

--- app/admin/post/new/page.tsx
+++ app/admin/post/new/page.tsx
@@ -1,13 +1,36 @@
 import { Button } from "@/app/components/button";
+import { BugIcon } from "lucide-react";
 
 export const dymic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
-    <div className="py-16 flex gap-4 flex-wrap">
-      <Button variant="default" size="sm">Funciona como o do JSX</Button>
-      <Button variant="ghost" size="md">Funciona como o do JSX</Button>
-      <Button variant="danger" size="lg">Funciona como o do JSX</Button>
+    <div>
+      <div className="py-16 flex gap-4 flex-wrap items-center">
+        <Button variant="default" size="sm">
+          <BugIcon /> Funciona como o do JSX
+        </Button>
+        <Button variant="ghost" size="md">
+          <BugIcon /> Funciona como o do JSX
+        </Button>
+        <Button variant="danger" size="lg">
+          <BugIcon /> Funciona como o do JSX
+        </Button>
+      </div>
+
+      <div className="py-16 flex gap-4 flex-wrap items-center">
+        <Button variant="default" size="sm" disabled>
+          <BugIcon /> Funciona como o do JSX
+        </Button>
+        <Button variant="ghost" size="md" disabled>
+          <BugIcon /> Funciona como o do JSX
+        </Button>
+        <Button variant="danger" size="lg" disabled>
+          <BugIcon /> Funciona como o do JSX
+        </Button>
+
+        
+      </div>
     </div>
   );
 }
