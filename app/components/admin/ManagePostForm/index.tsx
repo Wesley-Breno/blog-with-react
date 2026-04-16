b38f48b55d244fee8b80eac9e22b5106e31a0070
wesley <wesleybreno074@gmail.com>
2026-04-16 20:08:28 -0300

    class 189

--- app/components/admin/ManagePostForm/index.tsx
+++ app/components/admin/ManagePostForm/index.tsx
@@ -13,49 +13,72 @@ export function ManagePostForm() {
   return (
     <form action="" className="mb-16">
       <div className="flex flex-col gap-6">
-        <InputText
-          labelText="Nome"
-          placeholder="Digite seu nome"
-          type="password"
-        />
-
-        <ImageUploader/>
-
-        <InputText labelText="Sobrenome" placeholder="Digite seu sobrenome" />
-
-        <InputCheckbox labelText="Sobrenome" />
 
         <InputText
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
+          labelText="ID"
+          name="ID"
+          placeholder="ID gerado automaticamente"
+          type="text"
+          defaultValue={"asdasdasdas"}
+          readOnly
         />
 
-        <MarkdownEditor
-          disabled={false}
+        <InputText
+          labelText="Slug"
+          name="slug"
+          placeholder="Slug gerado automaticamente"
+          type="text"
+          defaultValue={"asdasdasdas"}
+          readOnly
+        />
+
+        <InputText
+          labelText="Autor"
+          name="author"
+          placeholder="Digite o nome do autor"
+          type="text"
+          defaultValue={""}
+        />
+
+
+        <InputText
+          labelText="Título"
+          name="title"
+          placeholder="Digite o título do post"
+          type="text"
+          defaultValue={""}
+        />
+
+        <InputText
+          labelText="Excerto"
+          name="excerpt"
+          placeholder="Digite o resumo"
+          type="text"
+          defaultValue={""}
+        />
+
+        <MarkdownEditor 
           labelText="Conteúdo"
-          textAreaName="content"
           value={contentValue}
           setValue={setContentValue}
+          textAreaName="content"
+          disabled={false}
         />
 
+        <ImageUploader />
 
         <InputText
-          disabled
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
+          labelText="URL da imagem de capa"
+          name="coverImageUrl"
+          placeholder="Digite a URL da imagem"
+          type="text"
+          defaultValue={""}
         />
-        <InputText
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          readOnly
-        />
-        <InputText
-          labelText="Sobrenome"
-          placeholder="Digite seu sobrenome"
-          defaultValue="Olá mundo"
-          readOnly
+
+        <InputCheckbox 
+          labelText="Publicar?"
+          name="published"
+          type="checkbox"
         />
 
         <div className="mt-4">
