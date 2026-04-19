e07927ac297b77ef8938d557f68111f4bbfa093b
wesley <wesleybreno074@gmail.com>
2026-04-18 23:36:12 -0300

    class 193

--- app/actions/post/create-post-action.ts
+++ app/actions/post/create-post-action.ts
@@ -12,11 +12,18 @@ export async function createPostAction(
     prevState: CreatePostActionState,
     formData: FormData
 ): Promise<CreatePostActionState> {
+    
+    if (!(formData instanceof FormData)) {
+        return {
+            formState: prevState.formState,
+            errors: ['Dados invalidos.'],
+        };
+    }
 
-    const title = formData.get("title")?.toString() || "";
+    const formDataToObj = Object.fromEntries(formData.entries());
 
     return {
-        formState: { ...prevState.formState, title },
+        formState: prevState.formState,
         errors: [],
     }
 }
