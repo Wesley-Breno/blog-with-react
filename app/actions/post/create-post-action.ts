ae4dbe625a45a2032bfc7a46a2b9e969a9efb732
wesley <wesleybreno074@gmail.com>
2026-05-05 00:08:43 -0300

    class 209

--- app/actions/post/create-post-action.ts
+++ app/actions/post/create-post-action.ts
@@ -1,6 +1,7 @@
 'use server';
 
 import { makePartialPublicPost, PublicPost } from '@/app/dto/post/dto';
+import { verifyLoginSession } from '@/app/lib/login/manage-login';
 import { PostCreateSchema } from '@/app/lib/post/validation';
 import { PostModel } from '@/app/models/post/post-model';
 import { postRepository } from '@/app/repositories/post';
@@ -20,6 +21,7 @@ export async function createPostAction(
   prevState: CreatePostActionState,
   formData: FormData,
 ): Promise<CreatePostActionState> {
+  const isAuthenticated = await verifyLoginSession();
 
   if (!(formData instanceof FormData)) {
     return {
@@ -30,6 +32,13 @@ export async function createPostAction(
 
   const formDataToObj = Object.fromEntries(formData.entries());
   const zodParsedObj = PostCreateSchema.safeParse(formDataToObj);
+
+  if (!isAuthenticated) {
+    return {
+      formState: makePartialPublicPost(formDataToObj),
+      errors: ['Faça login em outra aba antes de salvar.'],
+    };
+  }
 
   if (!zodParsedObj.success) {
     const errors = getZodErrorMessages(zodParsedObj.error.format());
